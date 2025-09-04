8b636e391b1b5b75e2ab3d7357629ee4cfd08df9
Prakhar Gupta <prakhargupta3680@gmail.com>
2025-09-05 00:04:04 +0530

    Added Sum

---
+++ 04/two_sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("\nEnter Num 1 : ");
+    scanf("%d", &a);
+
+    printf("\nEnter Num 2 : ");
+    scanf("%d", &b);
+
+    printf("\nThe Sum is : %d\n\n", a + b);
+
+    return 0;
+}
