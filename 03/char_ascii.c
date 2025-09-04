2fdd2232f3a2db315b03b7b417ed8f0d058560ef
Prakhar Gupta <prakhargupta3680@gmail.com>
2025-09-04 23:56:42 +0530

    added char_ascii.c

---
+++ 03/char_ascii.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char letter;
+    printf("\nEnter Letter : ");
+    scanf("%c", &letter);
+
+    printf("\nThe ASCII of %c is %d\n\n",letter,letter);
+
+    return 0;
+}
